518332c4da5659f65adde9cd3a87d73e64ccaec5
Anderson Castro <andersoncastrosantos@gmail.com>
2022-09-24 17:12:25 -0300

    commint 001

---
+++ src/main/java/br/com/empregos/CruddeempregosApplication.java
@@ -0,0 +1,13 @@
+package br.com.empregos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CruddeempregosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruddeempregosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/empregos/CruddeempregosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.empregos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddeempregosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
